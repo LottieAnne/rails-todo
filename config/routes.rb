7a5c8c52327f444beff0bb2f1eb9aee6096541bc
cchaae <kkang_chung125@yahoo.co.jp>
2022-09-29 18:29:41 +0900

    feat: Task 削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
   get "tasks", to: "tasks#index"
   get "tasks/new", to: "tasks#new"
   post "tasks/create", to: "tasks#create"
+  delete "tasks/:id", to: "tasks#destroy"
 end
